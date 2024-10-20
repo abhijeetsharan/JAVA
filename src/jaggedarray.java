880f9c4cb694b4884145a511332fb0af1aca6695
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2024-10-20 19:10:27 +0530

    pushing to github

---
+++ src/jaggedarray.java
@@ -0,0 +1,22 @@
+public class jaggedarray {
+    public static void main(String[] args) {
+        int[][] jaggedArr = new int[3][];
+
+        jaggedArr[0] = new int[2];
+        jaggedArr[1] = new int[3];
+        jaggedArr[2] = new int[4];
+
+        for(int i = 0; i<jaggedArr.length; i++){
+            for(int j = 0; j < jaggedArr[i].length; j++){
+                jaggedArr[i][j] = i + j;
+            }
+        }
+
+        for(int i = 0; i<jaggedArr.length; i++){
+            for(int j = 0; j< jaggedArr[i].length; j++){
+                System.out.print(jaggedArr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
