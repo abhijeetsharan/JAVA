81947a76308865be69e8d7e29ca1179c95912391
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2024-10-21 01:48:50 +0530

    pushing to github

---
+++ src/string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "Abhijeet";
+        String name1 = new String("Abhijeet Sharan");
+
+        System.out.println(name);
+        System.out.println(name1);
+        System.out.println(name.charAt(5));
+        System.out.println(name.concat(" Sharan"));
+    }
+}
