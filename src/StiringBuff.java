132df0fe7d7b3ee3d094f74f6583b0fc33864a6d
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2024-10-21 17:11:10 +0530

    pushing to github

---
+++ src/StiringBuff.java
@@ -0,0 +1,14 @@
+public class StiringBuff {
+    public static void main(String[] args) {
+        StringBuffer name = new StringBuffer("Abhijeet");
+        name.append(" Sharan");
+        System.out.println(name);
+
+        name.insert(9, "Java ");
+        System.out.println(name);
+
+        String sub = name.substring(5, 7);
+        System.out.println(sub);
+
+    }
+}
