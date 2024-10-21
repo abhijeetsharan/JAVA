f3df67f34705e3d0a5ffa29dcb1370b933835477
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2024-10-21 15:58:21 +0530

    pushing to github

---
+++ src/MutString.java
@@ -0,0 +1,12 @@
+class MutString {
+    public static void main(String[] args) {
+        String s = "Abhijeet";
+        s.concat("Sharan");
+        System.out.println(s);
+
+
+        String name = "Abhijeet";
+        name = name.concat(" Sharan");
+        System.out.println(name);
+    }
+}
